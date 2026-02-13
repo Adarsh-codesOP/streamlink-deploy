024b7d5c0f50d5afd1cfb7297b4fc6b308844bbc
Adarsh AS <adarshas107@gmail.com>
2026-02-13 14:51:03 +0530

    add ssl support for grpc client

--- signaling_layer/config.py
+++ signaling_layer/config.py
@@ -7,6 +7,7 @@ PORT = int(os.getenv("SIGNALING_PORT", 8001))
 # Management Layer Config (gRPC)
 MANAGEMENT_SERVICE_HOST = os.getenv("MANAGEMENT_HOST", "127.0.0.1")
 MANAGEMENT_SERVICE_PORT = int(os.getenv("MANAGEMENT_PORT", 50051))
+MANAGEMENT_SSL = os.getenv("MANAGEMENT_SSL", "false").lower() == "true"
 
 # Redis Config
 REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
